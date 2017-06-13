73f855b1a818747657239810563ab7ee436380e6
github username <github email address>
2017-06-12 21:13:44 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
+require_relative "./dog"
+require_relative "./fish"
+require_relative "./owner"
+
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,16 @@
+require_relative "./cat"
+require_relative "./dog"
+require_relative "./owner"
+
 class Fish
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+
 end
